9520084fbe3f9bca2adefb40f88565835eb1aadb
周文斌 <zhouwenbin@ctsi.com.cn>
2018-11-17 14:29:49 +0800

    最后修改

--- soft/guetzli/compress.bat
+++ soft/guetzli/compress.bat
@@ -1,22 +1,26 @@
-:: 这是一个图片压缩的批处理脚本，你只需要将图片所在的文件夹拖放使用本脚本打开，即可进行图片的批量压缩。
-:: 图片压缩使用的是google开源工具guetzli,可配置参数只有一个“quality”（质量,本程序中可选值范围85~100）
 @echo off
-:: 图片压缩质量
-set QUALITY=90
+rem 这是一个图片压缩的批处理脚本，你只需要将图片所在的文件夹拖放使用本脚本打开，即可进行图片的批量压缩。
+rem 图片压缩使用的是google开源工具guetzli,可配置参数只有一个“quality”（质量,本程序中可选值范围85~100）
+rem 图片压缩质量
+set QUALITY=85
 setlocal enabledelayedexpansion
 set p=%~dp0
-set COMPRESS_DIR=%p%CompressedImage
+
 for /f "delims=" %%i in ('dir /a-d /s /b') do (
      if "%%~xi" == ".JPG" ren "%%~fsi" "%%~ni.jpg"
      if "%%~xi" == ".jpeg" ren "%%~fsi" "%%~ni.jpg"
      if "%%~Xi" == ".JPEG" ren "%%~fsi" "%%~ni.jpg"
 )
-mkdir %COMPRESS_DIR%
 if "%PROCESSOR_ARCHITECTURE%%PROCESSOR_ARCHITEW6432%" == "x86" (
-    set compressor=%p%\x86\guetzli
+    set compressor=%p%x86\guetzli
 ) else (
-    set compressor=%p%\x86-64\guetzli
+    set compressor=%p%x86-64\guetzli
 )
+set parent_dir=%~dp1
+for %%i in (dir %1) do set name=%%~nxi
+set COMPRESS_DIR=%parent_dir%!name!-压缩副本
+mkdir %COMPRESS_DIR%
+echo 图片压缩后将被保存到"%COMPRESS_DIR%"
 
 echo 开始批量压缩全部图片:
 echo.
@@ -27,15 +31,15 @@ echo.
 
 (dir %1 /aa /b /s | findstr /e /c:"jpg") >tmp.txt
 for /f "delims=" %%i in (tmp.txt) do (
-    echo "%%~fsi(正在压缩...)"
+    echo "%%~fsi"正在压缩...
     %compressor% --quality %QUALITY% --verbose %%~fsi %COMPRESS_DIR%\%%~ni.jpg
-    echo "%%~fsi(压缩完成)"
+    echo "%%~fsi"压缩完成
 )
 (dir %1 /aa /b /s | findstr /e /c:"png") >tmp.txt
 for /f "delims=" %%i in (tmp.txt) do (
-    echo "%%~fsi(正在压缩...)"
+    echo "%%~fsi"正在压缩...
     %compressor% --quality %QUALITY% --verbose %%~fsi %COMPRESS_DIR%\%%~ni.jpg
-    echo "%%~fsi(压缩完成)"
+    echo "%%~fsi"压缩完成
 )
 echo.
 echo.
